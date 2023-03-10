72db1d5aeb17ae0a8e991210addf1612aba1cf43
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2023-03-10 13:37:26 +0200

    Add alert messages

--- frontend/src/pages/SignIn.vue
+++ frontend/src/pages/SignIn.vue
@@ -1,75 +1,89 @@
 <template>
-   <p>{{ message }}</p>
   <div class="container">
-    <div v-if="getError" class="error">
-   <p>{{ getError }}</p>
-</div>
-      <h1 class="sm-title"></h1>
-      <div class="sm-card">
-          <div v-if="isLoggedIn">
-            You're already logged in 
-              <button @click="logoutUser" class="logout-button" >Logout</button>
-          </div>
-          <div v-else>
-              <h3>Login!</h3>
-              <form @submit="onLogin" class="login-form">
-                  <input class="login-form-email" type="text" v-model="loginEmail" placeholder="Email" />
-                  <br />
-                  <input class="login-form-password" type="password" v-model="loginPassword" placeholder="Password" />
-                  <br />
-                  <br />
-                  <input type="submit" value="Login" class="login-form-submit" />
-              </form>
-          </div>
+    <div v-if="getError || message">
+    <div v-if="isModalVisible" class="alert danger">
+      <p>{{ getError }}</p>
+      <p>{{ message }}</p>
+      <span type="button" class="close" 
+      @click="closeModal"> X 
+    </span>
+    </div>
+  </div>
+
+    <h1 class="sm-title"></h1>
+    <div class="sm-card">
+      <div v-if="isLoggedIn">
+        You're already logged in
+        <button @click="logoutUser" class="logout-button">Logout</button>
       </div>
+      <div v-else>
+        <h3>Login!</h3>
+        <form @submit="onLogin" class="login-form">
+          <input class="login-form-email" type="text" v-model="loginEmail" placeholder="Email" />
+          <br />
+          <input class="login-form-password" type="password" v-model="loginPassword" placeholder="Password" />
+          <br />
+          <br />
+          <input type="submit" value="Login" @click="showModal" class="login-form-submit" />
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import "@/store/index.js";
-import { mapActions, mapGetters } from "vuex";
-export default {
-  computed: {
+  import "@/store/index.js";
+  import { mapActions, mapGetters } from "vuex";
+  export default {
+    computed: {
       ...mapGetters(["getAuthToken", "getUserEmail", "getUserID", "isLoggedIn", "getError"]),
-   },
- 
-  data() {
+    },
+
+    data() {
       return {
-      loginEmail: "",
-      loginPassword: "",
-      message: "",
+        loginEmail: "",
+        loginPassword: "",
+        message: "",
+        isModalVisible: true,
       };
-  },
-  methods: {
+    },
+    methods: {
       ...mapActions(["loginUser", "logoutUser"]),
-  
+
+     closeModal() {
+       this.isModalVisible = false;
+     },
+
+     showModal() {
+        this.isModalVisible = true;
+       },
+   
+
       onLogin(event) {
         event.preventDefault();
 
-        if (this.loginEmail == "" || this.loginPassword == ""){
-            this.message = "error: empty password " 
-            return
-          } 
-       
-          let data = {
-              user: {
-                  email: this.loginEmail,
-                  password: this.loginPassword,
-              },
-          };
-       
-          this.loginUser(data);
-          this.resetData();
-      
-      },
+        if (this.loginEmail == "" || this.loginPassword == "") {
+          this.message = "Empty Email or password"
+          return
+        }
 
+        let data = {
+          user: {
+            email: this.loginEmail,
+            password: this.loginPassword,
+          },
+        };
+
+        this.loginUser(data);
+        this.resetData();
+      },
 
       resetData() {
-          this.signUpEmail = "";
-          this.signUpPassword = "";
-          this.loginEmail = "";
-          this.loginPassword = "";
+        this.signUpEmail = "";
+        this.signUpPassword = "";
+        this.loginEmail = "";
+        this.loginPassword = "";
       },
-  },
-}
+    },
+  }
 </script>
