e5d84bd782e2718935634fc14b5e0808d786149a
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2023-03-31 14:58:41 +0300

    Fixing controllers adter rubcop

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,60 +1,58 @@
 # frozen_string_literal: true
 
-module Api
-  module V1
-    class TasksController < ApplicationController
-      include MessageRenderHelper
+module Api::V1
+  class TasksController < ApplicationController
+    include MessageRenderHelper
 
-      before_action :authenticate_user!
-      before_action :set_task, only: %i[show update destroy]
+    before_action :authenticate_user!
+    before_action :set_task, only: %i[show update destroy]
 
-      def show
-        return errors("Task not found") unless @task
+    def show
+      return errors("Task not found") unless @task
 
-        success(V1::TaskBlueprint.render_as_hash(@task, view: :show))
-      end
+      success(V1::TaskBlueprint.render_as_hash(@task, view: :show))
+    end
 
-      def index
-        @tasks = Task.all
-        @task_count = @tasks.size
-        success_with_meta(V1::TaskBlueprint.render_as_hash(@tasks, view: :index), meta: { total: @task_count, link: api_v1_tasks_url })
-      end
+    def index
+      @tasks = Task.all
+      @task_count = @tasks.size
+      success_with_meta(V1::TaskBlueprint.render_as_hash(@tasks, view: :index), meta: { total: @task_count, link: api_v1_tasks_url })
+    end
 
-      def assigned
-        @tasks = Task.all.where(assignee_id: current_user.id)
-        @task_count = @tasks.size
-        success_with_meta(V1::TaskBlueprint.render_as_hash(@tasks, view: :index), meta: { total: @task_count, link: assigned_api_v1_tasks_url })
-      end
+    def assigned
+      @tasks = Task.all.where(assignee_id: current_user.id)
+      @task_count = @tasks.size
+      success_with_meta(V1::TaskBlueprint.render_as_hash(@tasks, view: :index), meta: { total: @task_count, link: assigned_api_v1_tasks_url })
+    end
 
-      def creator
-        @tasks = current_user.tasks
-        @task_count = @tasks.size
-        success_with_meta(V1::TaskBlueprint.render_as_hash(@tasks, view: :index), meta: { total: @task_count, link: creator_api_v1_tasks_url })
-      end
+    def creator
+      @tasks = current_user.tasks
+      @task_count = @tasks.size
+      success_with_meta(V1::TaskBlueprint.render_as_hash(@tasks, view: :index), meta: { total: @task_count, link: creator_api_v1_tasks_url })
+    end
 
-      def create
-        @task = current_user.tasks.create!(tasks_params)
-        return errors @task.errors unless @task.save
+    def create
+      @task = current_user.tasks.create!(tasks_params)
+      return errors @task.errors unless @task.save
 
-        success(V1::TaskBlueprint.render_as_hash(@task, view: :show))
-      end
+      success(V1::TaskBlueprint.render_as_hash(@task, view: :show))
+    end
 
-      def update
-        @task.update!(tasks_params)
-        return errors @task.errors unless @task.save
+    def update
+      @task.update!(tasks_params)
+      return errors @task.errors unless @task.save
 
-        success(V1::TaskBlueprint.render_as_hash(@task, view: :show))
-      end
+      success(V1::TaskBlueprint.render_as_hash(@task, view: :show))
+    end
 
-      private
+    private
 
-      def tasks_params
-        params.require(:task).permit(:status, :description, :assignee_id)
-      end
+    def tasks_params
+      params.require(:task).permit(:status, :description, :assignee_id)
+    end
 
-      def set_task
-        @task = Task.find(params[:id])
-      end
+    def set_task
+      @task = Task.find(params[:id])
     end
   end
 end
--- app/controllers/api/v1/teams_controller.rb
+++ app/controllers/api/v1/teams_controller.rb
@@ -1,44 +1,42 @@
 # frozen_string_literal: true
 
-module Api
-  module V1
-    class TeamsController < ApplicationController
-      include MessageRenderHelper
+module Api::V1
+  class TeamsController < ApplicationController
+    include MessageRenderHelper
 
-      before_action :authenticate_user!
-      before_action :set_team, only: %i[show update destroy]
-      before_action :authenticate_access, only: %i[create update destroy]
+    before_action :authenticate_user!
+    before_action :set_team, only: %i[show update destroy]
+    before_action :authenticate_access, only: %i[create update destroy]
 
-      def index
-        @teams = Team.where(id: current_user.team_id)
-        render json: @teams
-      end
+    def index
+      @teams = Team.where(id: current_user.team_id)
+      render json: @teams
+    end
 
-      def show
-        render json: @team
-      end
+    def show
+      render json: @team
+    end
 
-      def create
-        @team = Team.create!(team_params)
-        render json: @team
-      end
+    def create
+      @team = Team.create!(team_params)
+      render json: @team
+    end
 
-      private
+    private
 
-      def set_team
-        @team = Team.find(params[:id])
-      end
+    def set_team
+      @team = Team.find(params[:id])
+    end
 
-      def team_params
-        params.require(:team).permit(:name)
-      end
+    def team_params
+      params.require(:team).permit(:name)
+    end
 
-      def authenticate_access
-        return if current_user.admin? || current_user.project_manager?
+    def authenticate_access
+      return if current_user.admin? || current_user.project_manager?
 
-        render status: :unauthorized, \
-               json: { error: "You don't have access to creating teams! Contact your local support." }
-      end
+      render status: :unauthorized, \
+             json: { error: "You don't have access to creating teams! Contact your local support." }
     end
   end
 end
