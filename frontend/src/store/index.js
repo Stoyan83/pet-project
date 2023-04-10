29778dd1eabc177e096d0d67f06374d1e515bb2b
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2023-04-10 10:23:15 +0300

    Add task manager to vue store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,12 +2,14 @@ import { createStore } from 'vuex'
 import sessionManager from "@/store/modules/sessionManager";
 import projectManager from "@/store/modules/projectManager";
 import teamManager from "@/store/modules/teamManager";
+import taskManager from "@/store/modules/taskManager";
 
 
 export default createStore({
   modules: {
     sessionManager,
     projectManager,
-    teamManager
+    teamManager,
+    taskManager
   }
 })
---
+++ frontend/src/store/modules/taskManager.js
@@ -0,0 +1,105 @@
+import axios from "axios";
+import router from "@/router";
+
+const API_URL = "http://localhost:3000/api/v1/tasks";
+
+const state = {
+  tasks: [],
+  task: null,
+};
+
+const getters = {
+  allTasks: (state) => state.tasks,
+  getTask: (state) => state.task,
+};
+
+const actions = {
+
+  async fetchTasks({ commit }) {
+    try {
+    const response = await axios.get(`${API_URL}`);
+    commit("setTasks", response.data);
+    } catch(e) {
+      console.error(e.response.data)
+    }
+  },
+
+  async fetchTask({ commit }, id) {
+    try {
+    const response = await axios.get(`${API_URL}/${id}`);
+    commit("setTask", response.data);
+    } catch(e) {
+      console.error(e.response.data.status)
+      if(e.response.data.status == 404) {
+        router.back()
+      }
+    }
+  },
+
+  async addTask({ commit }, task) {
+    try {
+    const response = await axios.post(`${API_URL}`,
+      {
+        status: task.status,
+        description: task.description,
+        team_id: task.team_id,
+        user_id: task.user_id,
+        assignee_id: task.asignee_id,
+      }
+    );
+    commit("newTask", response.date);
+    router.replace({'query': null})
+    } catch(e) {
+      console.error(e.response.data)
+    }
+  },
+
+
+
+  async deleteTask({ commit }, id) {
+    try {
+    await axios.delete(`${API_URL}/${id}`);
+    commit("removeTask", id);
+    } catch(e) {
+      console.error(e.response.data.status)
+    }
+  },
+
+  async updateTask({ commit }, updatedTask) {
+    try {
+    await axios.put(
+      API_URL + `/${updatedTask.id}`,
+      updatedTask
+    );
+    commit("setUpdatedTask", updatedTask);
+  }  catch(e) {
+    console.error(e.response.data.statu)
+  }
+
+  },
+};
+
+const mutations = {
+  setTasks: (state, tasks) => (state.tasks = tasks),
+  setTask: (state, task) => (state.task = task),
+  newTask: (state, task) => (state.tasks.data.push(task)),
+  removeTask (state, id ) {
+    let index = state.tasks.data.findIndex(task => task.id == id);
+    state.tasks.data.splice(index, 1)
+  },
+  setUpdatedTask: (state, updatedTask) => {
+    const index = state.tasks.data.findIndex(
+      (task => task.id === updatedTask.id)
+    );
+    if (index !== -1) {
+      state.tasks.splice(index, 1, updatedTask);
+    }
+  },
+};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations,
+};
