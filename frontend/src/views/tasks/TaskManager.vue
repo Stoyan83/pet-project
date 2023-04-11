2687657574532038679e7490e0f0fa34279e313d
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2023-04-11 08:37:12 +0300

    Fixing draggable

--- frontend/src/views/tasks/TaskManager.vue
+++ frontend/src/views/tasks/TaskManager.vue
@@ -1,24 +1,6 @@
 <template>
-
-  <!-- <div v-if="isLoggedIn">
-    <div v-if="allTasks.data">
-      <h3>Total Tasks: {{ allTasks.meta.total }}</h3>
-      <div class="projects">
-      <div  v-for="data in allTasks.data" :key="data">
-          <div @click=" onClick(data.id)" class="project-type">
-            {{ data.status }}
-          </div>
-          <div class="project-description">
-            {{ data.description }}
-            <i @click="deleteTask(data.id)" class="fas fa-trash-alt"></i>
-          </div>
-        </div>
-      </div>
-      </div>
-  </div>
-  <router-view :key="$route.fullPath"></router-view>
-  <side-bar v-if="isLoggedIn"></side-bar> -->
- <h3>Total Tasks: {{ allTasks.meta.total }}</h3>
+<div v-if="allTasks.data">
+  <h3>Total Tasks: {{ allTasks.meta.total }}</h3>
 <div class="projects">
   <draggable v-model="allTasks.data" item-key="id">
     <template #item="{ element }">
@@ -26,19 +8,22 @@
         Task: {{ element.description }}
       </div>
     </template>
-</draggable>
+  </draggable>
+  </div>
 </div>
+<router-view :key="$route.fullPath"></router-view>
+<side-bar v-if="isLoggedIn"></side-bar> 
 </template>
 
 <script>
   import { mapGetters, mapActions } from 'vuex';
-  // import SideBar from '@/components/layouts/SideBar.vue'
+  import SideBar from '@/components/layouts/SideBar.vue'
   import router from '@/router';
   import draggable from "vuedraggable";
 
   export default {
     components: {
-      // SideBar,
+      SideBar,
       draggable,
     },
     data() {
