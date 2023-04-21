6c332508d57ccd7d34c785316882eb23573e9760
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2023-04-21 15:46:02 +0300

    Not Found page style

--- frontend/src/components/layouts/NotFound.vue
+++ frontend/src/components/layouts/NotFound.vue
@@ -1,3 +1,37 @@
 <template>
-  Not Found!
+  <div class="not-found">
+    <h1>404</h1>
+    <h2>Oops! Page Not Found</h2>
+    <p>Sorry, but the page you are looking for does not exist.</p>
+    <router-link to="/" class="backlink">Go back to homepage</router-link>
+  </div>
 </template>
+
+<style scoped>
+.not-found {
+  text-align: center;
+  margin-top: 100px;
+  font-size: 3rem;
+}
+
+.not-found p {
+  font-size: 1.2rem;
+  margin-bottom: 40px;
+}
+
+.backlink {
+  display: inline-block;
+  padding: 10px 20px;
+  border: 2px solid #2b3b49;
+  background-color: #2b3b49;
+  color: #fff;
+  font-size: 1.2rem;
+  text-decoration: none;
+  transition: all 0.2s ease-in-out;
+}
+
+.back-link:hover {
+  background-color: #fff;
+  color: #2b3b49;
+}
+</style>
