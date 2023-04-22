6a8c80c3a94efbddee1dd844f3f6cd3d60018135
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2023-04-22 15:54:27 +0300

    Css for the Team

--- frontend/src/views/teams/TeamDetail.vue
+++ frontend/src/views/teams/TeamDetail.vue
@@ -1,3 +1,90 @@
 <template>
-  Test detail...
+  <div class="team-detail">
+    <div v-if="getTeam" class="frame">
+      <div class="frame-header"></div>
+      <div class="team-info">
+        <div class="team-name">{{ getTeam.name }}</div>
+        <div class="team-id">Team ID: {{ getTeam.id }}</div>
+      </div>
+      <div class="frame-footer"></div>
+    </div>
+    <div v-else class="loading">Loading team details...</div>
+  </div>
 </template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+  name: "TeamDetail",
+  props: {
+    teamId: {
+      type: Number,
+      required: true,
+    },
+  },
+  computed: {
+    ...mapGetters(['getTeam']),
+  },
+  methods: {
+    ...mapActions(['fetchTeam']),
+  },
+  mounted() {
+    this.fetchTeam(this.teamId);
+  },
+};
+</script>
+
+
+<style scoped>
+.team-detail {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f0f2f5;
+}
+
+.frame {
+  background-color: #fff;
+  border: 10px solid #a8a8a8;
+  box-shadow: 0 0 10px #a8a8a8;
+  width: 80%;
+  height: 80%;
+  position: relative;
+}
+
+.frame-header,
+.frame-footer {
+  height: 50px;
+  background-color: #2c3e50;
+  color: #fff;
+}
+
+.frame-header {
+  border-bottom: 10px solid #1c1e21;
+}
+
+.frame-footer {
+  border-top: 10px solid #1c1e21;
+}
+
+.team-info {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  padding: 20px;
+}
+
+.team-name {
+  font-size: 36px;
+  font-weight: bold;
+  margin-bottom: 20px;
+}
+
+.team-id {
+  font-size: 24px;
+}
+</style>
--- frontend/src/views/teams/TeamManager.vue
+++ frontend/src/views/teams/TeamManager.vue
@@ -1,60 +1,111 @@
 <template>
-    <div v-if="isLoggedIn">
-      <div  v-if="allTeams.length > 0" class="projects">
-
-      <div  v-for="team in allTeams" :key="team.id">
-
-          <div @click="onClick(team.id)" class="project-type">
-            {{ team.id }}
-          </div>
-          <div class="project-description">
-              {{ team.name }}
-              <!-- <i @click="deleteProject(data.id)" class="fas fa-trash-alt"></i> -->
+  <div v-if="isLoggedIn">
+    <div v-if="allTeams.length > 0" class="team-list">
+      <div v-for="team in allTeams" :key="team.id" class="team">
+        <div class="team-header" @click="onClick(team.id)">
+          <div class="team-name">{{ team.name }}</div>
+          <div class="arrow-icon" :class="{ 'rotate': showTeamDetail && selectedTeamId === team.id }">
+            <i class="fas fa-chevron-down"></i>
           </div>
         </div>
-    </div>
-      <div v-else>
-        <h3>You have no team</h3>
+        <div class="team-details" v-show="showTeamDetail && selectedTeamId === team.id">
+          <div class="team-description">{{ team.description }}</div>
+        </div>
       </div>
+    </div>
+    <div v-else>
+      <h3>You have no team</h3>
+    </div>
+    <team-detail v-if="showTeamDetail" :team-id="selectedTeamId" @close="showTeamDetail = false"></team-detail>
   </div>
 </template>
 
-
-
 <script>
-import { mapGetters, mapActions } from 'vuex';
-// import SideBar from '@/components/layouts/SideBar.vue'
-import router from '@/router';
-export default {
-  // components: {
-  //   SideBar,
-  // },
-
-  name: "ProjectManager",
-  methods: {
-    ...mapActions([
-      'fetchTeams',
-    ]),
-
-    onClick(id) {
-      router.push(this.$route.path + '/' + id )
+  import { mapGetters, mapActions } from "vuex";
+  import TeamDetail from "./TeamDetail.vue";
+  export default {
+    components: {
+      TeamDetail,
     },
 
-  },
+    name: "ProjectManager",
+    data() {
+      return {
+        selectedTeamId: null,
+        showTeamDetail: false,
+      };
+    },
 
-  computed: {
-    ...mapGetters([
-      'allTeams',
-      'isLoggedIn',
-    ]),
-  },
+    methods: {
+      ...mapActions(["fetchTeams"]),
 
-  mounted() {
-    this.fetchTeams();
-  }
-}
+      onClick(id) {
+        this.selectedTeamId = id;
+        if (this.selectedTeamId === id) {
+          this.showTeamDetail = !this.showTeamDetail; // toggle
+        } else {
+          this.selectedTeamId = id;
+          this.showTeamDetail = true;
+        }
+      },
+    },
+
+    computed: {
+      ...mapGetters(["allTeams", "isLoggedIn"]),
+    },
+
+    mounted() {
+      this.fetchTeams();
+    },
+  };
 </script>
 
-<style>
+<style scoped>
+  .team-list {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    padding-top: 20px;
+  }
 
+  .team {
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    overflow: hidden;
+    transition: all 0.3s ease-in-out;
+  }
+
+  .team:hover {
+    box-shadow: 0px 0px 10px #ccc;
+  }
+
+  .team-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+    background-color: #f5f5f5;
+    cursor: pointer;
+  }
+
+  .team-name {
+    font-weight: bold;
+  }
+
+  .arrow-icon {
+    transform-origin: center;
+    transition: transform 0.3s ease-in-out;
+  }
+
+  .rotate {
+    transform: rotate(180deg);
+  }
+
+  .team-details {
+    padding: 10px;
+  }
+
+  .team-description {
+    white-space: pre-wrap;
+  }
 </style>
